48038f7be8fa5d784c2109adedf3bee838b8c4d4
Matthew Talluto <mtalluto@gmail.com>
2018-11-09 16:54:44 +0100

    Finished second day and most of day 3

--- code/1_iii_likelihood.r
+++ code/1_iii_likelihood.r
@@ -3,27 +3,27 @@ trees <- readRDS("data/trees.rds")
 dat <- trees[grepl("TSU-CAN", species) & year == 2005 & n > 0]
 
 lik_func <- function(theta, n, k) {
-	-sum(dbinom(n, k, theta, log=TRUE))
+	sum(dbinom(n, k, theta, log=TRUE))
 }
 
-optim(0.5, lik_func, n=dat$died, k=dat$n, method="Brent", lower=0, upper=1)
+optim(0.5, lik_func, n=dat$died, k=dat$n, method="Brent", lower=0, upper=1, control=list(fnscale=-1))
 
 par(mar=c(4.5,4.5,0.2,0.2))
-plot(seq(0,1,0.01), sapply(seq(0,1,0.01), lik_func, n=dat$died, k=dat$n), type='l', col='#0066cc', bty='n', xlab=expression(theta), ylab="Negative Log Likelihood", lwd=3.5)
+plot(seq(0,1,0.01), sapply(seq(0,1,0.01), lik_func, n=dat$died, k=dat$n), type='l', col='#0066cc', bty='n', xlab=expression(theta), ylab="Log Likelihood", lwd=3.5)
 abline(v=0.147293, lty=2)
 abline(h=lik_func(0.147293, n=dat$died, k=dat$n), lty=2)
 text(0.147293, 4000, expression(paste(theta, "=", "0.147")), pos=4)
 
-nll_count1 <- function(lambda, n) {
-	-sum(dpois(n, lambda, log=TRUE))
+ll_count1 <- function(lambda, n) {
+	sum(dpois(n, lambda, log=TRUE))
 }
 
-nll_count2 <- function(musize, n) {
-	-sum(dnbinom(n, mu=musize[1], size=musize[2], log=TRUE))
+ll_count2 <- function(musize, n) {
+	sum(dnbinom(n, mu=musize[1], size=musize[2], log=TRUE))
 }
 
-res1 <- optim(5, nll_count1, n=dat$n, lower=0, upper=max(dat$n), method='Brent')
-res2 <- optim(c(5, 5), nll_count2, n=dat$n)
+res1 <- optim(5, ll_count1, n=dat$n, lower=0, upper=max(dat$n), method='Brent',control=list(fnscale=-1))
+res2 <- optim(c(5, 5), ll_count2, n=dat$ncontrol=list(fnscale=-1))
 
 plot(xx, sapply(xx, nll_count1, n=dat$n) , type='l', col='#0066cc', bty='n', xlab=expression(lambda), ylab="Negative Log Likelihood", lwd=3.5)
 
@@ -41,3 +41,5 @@ hist(sim2, xlim=range(dat$n), freq=FALSE, col="#c6b6e5", main= "Negative Binomia
 lines(density(dat$n), col="#c96840", lwd=4)
 text(15, 0.06, paste("mean =", round(res2$par[1], 3), "\nvariance =", round(res2$par[1] + res2$par[1]^2/res2$par[2], 3)), pos=4, cex=1.3, col="#7e54bf")
 text(15, 0.05, paste("mean =", round(mean(dat$n), 3), "\nvariance =", round(var(dat$n), 3)), pos=4, cex=1.3, col="#c96840")
+
+
