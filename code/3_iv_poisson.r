c9c0fd2da868040fd7951013bf70e92c8f398173
Matthew Talluto <mtalluto@gmail.com>
2018-11-16 10:16:08 +0100

    Updated poisson

--- code/3_iv_poisson.r
+++ code/3_iv_poisson.r
@@ -1,6 +1,7 @@
 library(data.table)
 library(rstan)
 library(bayesplot)
+options(mc.cores = parallel::detectCores())
 
 trees <- readRDS("data/trees.rds")
 dat <- trees[grepl("ACE-SAC", species) & n+born > 0]
@@ -11,9 +12,9 @@ plot(n ~ annual_mean_temp, data=dat, pch=16, cex=0.3)
 standata <- list(n = nrow(dat), ntrees = dat$n, 
 	temperature = dat$annual_mean_temp)
 model <- stan("code/3_iv_poisson.stan", data = standata, 
-	iter=2000, chains=1)
+	iter=4000, chains=4)
 
-samples <- as.matrix(model, pars=c("intercept", "slope"))
+samples <- as.array(model, pars=c("intercept", "slope"))
 yhats <- as.matrix(model, pars = "lambda")
 mcmc_trace(samples)
 
@@ -24,3 +25,15 @@ polygon(c(dat$annual_mean_temp[ord], rev(dat$annual_mean_temp[ord])),
 	c(ci[1, ord], rev(ci[2, ord])), col="#ff000066", border=NA)
 lines(dat$annual_mean_temp[ord], colMeans(yhats)[ord], 
 	col='red', lwd=4)
+
+mcmc_hist(samples)
+mcmc_hist_by_chain(samples)
+mcmc_dens_overlay(samples)
+mcmc_violin(samples)
+
+mcmc_intervals(samples, pars="intercept")
+mcmc_areas(samples)
+
+mcmc_scatter(samples)
+mcmc_pairs(samples)
+mcmc_combo(samples, combo = c("dens", "trace"))
